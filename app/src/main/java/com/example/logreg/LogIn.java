9a4f8bd3e050fccf73b67937ffbfb70aaf37da88
viraj1227 <virajthacker1227@gmail.com>
2022-01-06 20:00:15 +0530

    Registration

--- app/src/main/java/com/example/logreg/LogIn.java
+++ app/src/main/java/com/example/logreg/LogIn.java
@@ -6,14 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
-    private Button ba;
+    private TextView ba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        ba=findViewById(R.id.Log_In);
+        ba=findViewById(R.id.back);
         ba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
